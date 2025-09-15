35ea3208316a4e72f681fc900ddd84eb63a961c7
Nahid Hasan Utsob <mdnahidhassan555@gmail.com>
2025-09-15 09:57:27 +0600

    added menu button

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -4,19 +4,19 @@ import Button from "../Button/Button";
 
 const Hero = () => {
   return (
-    <section className="max-w-7xl  mx-auto mt-28">
-      <section className="flex items-center justify-between lg:flex-row flex-col">
+    <section className="max-w-[1400px]   mx-auto mt-28">
+      <section className="flex items-center justify-between flex-col lg:flex-row">
         <div className="flex-1 flex-col gap-4 justify-between">
           <div className="mb-3">
             <p
-              className="text-lg py-2 px-5
+              className="text-md py-2 px-5
                   text-orange-500  bg-orange-200 inline rounded-full text-center "
             >
               Export Best Quality...
             </p>
           </div>
           <p
-            className="lg:text-6xl text-5xl font-bold leading-[1.1] lg:mb-3 mb-2
+            className="md:text-6xl text-5xl font-bold leading-[1.1] md:mb-3 mb-2
 "
           >
             Tasty Organic <br /> <span className="text-orange-500">Fruits</span>{" "}
--- src/Component/NavBar/NavBar.jsx
+++ src/Component/NavBar/NavBar.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { FaShoppingCart } from "react-icons/fa";
 import { RiMenu3Line } from "react-icons/ri";
-
 import { FaHeart } from "react-icons/fa";
+import { RiMenu4Fill } from "react-icons/ri";
 
 const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <section>
       {/* navbar */}
       <header className="fixed top-0 left-0 right-0 ">
-        <nav className=" flex justify-between items-center lg:h-20 h-15 fixed top-0 left-0 right-0 max-w-7xl  mx-auto bg-white">
+        <nav className=" flex justify-between items-center md:h-20 h-15 fixed top-0 left-0 right-0 max-w-[1400px]  mx-auto bg-white">
           <div className="flex justify-between h-14 items-center px-4 ">
             <p className="text-3xl font-semibold tracking-wider">
               Gr
@@ -18,7 +24,7 @@ const NavBar = () => {
               cify{" "}
             </p>
           </div>
-          <ul className="lg:flex gap-x-13 tracking-wider hidden ">
+          <ul className="md:flex gap-x-13 tracking-wider hidden ">
             <li>
               <a href="" className="text-base text-orange-600 ">
                 Home
@@ -42,11 +48,49 @@ const NavBar = () => {
           </ul>
 
           {/* mobile view */}
-          <div className="mr-5 text-lg lg:hidden block">
-            <RiMenu3Line className="text-3xl" />
-          </div>
 
-          <div className="lg:flex items-center justify-between gap-3 hidden">
+          <ul
+            className={`flex flex-col bg-orange-500/15 backdrop-blur-3xl p-12 absolute top-30 -left-full transform -translate-x-1/2 gap-y-9 rounded-2xl tracking-wider md:hidden transition-all duration-500 ${
+              showMenu ? "left-1/2" : ""
+            }  `}
+          >
+            <li>
+              <a href="" className="text-base text-orange-600 ">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-base hover:text-orange-600">
+                About Us
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-base hover:text-orange-600">
+                Process
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-base hover:text-orange-600">
+                Contact Us
+              </a>
+            </li>
+
+            <li className="rounded-full w-fit border-2 border-orange-500 flex items-center p-1">
+              <input
+                type="text"
+                placeholder="Search..."
+                className="p-2 focus:outline-none focus:ring-0 text-black"
+              />
+              <button className="bg-orange-500 flex justify-center items-center ml-auto p-3 rounded-[50%] text-white font-bold cursor-pointer">
+                <FaSearch />
+              </button>
+            </li>
+          </ul>
+          <button className="text-3xl md:hidden block" onClick={handleMenu}>
+            {showMenu ? <RiMenu4Fill /> : <RiMenu3Line />}
+          </button>
+
+          <div className="md:flex items-center justify-between gap-3 hidden">
             <div className="rounded-full w-fit border-2 border-orange-500 flex items-center p-1">
               <input
                 type="text"
